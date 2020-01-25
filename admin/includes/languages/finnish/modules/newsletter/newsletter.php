971a6d3f57543a59add8c48f7bd290b682f6b1c2
İhsan Yüz <60288896+krbuk@users.noreply.github.com>
2020-01-25 14:27:58 +0200

    Add files via upload

--- admin/includes/languages/finnish/modules/newsletter/newsletter.php
+++ admin/includes/languages/finnish/modules/newsletter/newsletter.php
@@ -1,1 +1,38 @@
+<?php
+/**
+ * @package admin
+ * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: newsletter.php 4385 2006-09-04 04:10:48Z drbyte $
+ */
+// Finnish translation by JNT Finland Oy (Pasi Rajala) <pasi.rajala@jnt-finland.fi>
+// Finnish translation Ver. 1.5.5 by Nida Verkkopalvelu Oy (Ihsan Yuz) <ihsan@nida.fi> 
 
+define('TEXT_COUNT_CUSTOMERS', 'Uutiskirjeen vastaanottavia asiakkaita: %s');
+define('HEADING_TITLE', 'Uutiskirjeen hallinta');
+
+define('TABLE_HEADING_NEWSLETTERS', 'Uutiskirjeet');
+define('TABLE_HEADING_SIZE', 'Koko');
+define('TABLE_HEADING_MODULE', 'Moduli');
+define('TABLE_HEADING_SENT', 'Lähetetty');
+define('TABLE_HEADING_STATUS', 'Tila');
+define('TABLE_HEADING_ACTION', 'Toiminto');
+
+define('TEXT_NEWSLETTER_MODULE', 'Moduli:');
+define('TEXT_NEWSLETTER_TITLE', 'Aihe:');
+define('TEXT_NEWSLETTER_CONTENT', 'Muotoilematon <br>teksti:');
+define('TEXT_NEWSLETTER_CONTENT_HTML', 'Muotoiltu <br>teksti:');
+
+define('TEXT_NEWSLETTER_DATE_ADDED', 'Lisäyspäivämäärä:');
+define('TEXT_NEWSLETTER_DATE_SENT', 'Lähetyspäivämäärä:');
+
+define('TEXT_INFO_DELETE_INTRO', 'Oletko varma että haluat poistaa tämän uutiskirjeen?');
+
+define('TEXT_PLEASE_SELECT_AUDIENCE','Valitse kenelle tämä uutiskirje lähetetään: ');
+define('TEXT_PLEASE_WAIT', 'Odota ... postia lähetetään ...<br><br> Ãlä keskeytä prosessia!');
+define('TEXT_FINISHED_SENDING_EMAILS', 'Sähköpostit lähetetty!');
+
+define('ERROR_NEWSLETTER_TITLE', 'Virhe: Uutiskirjeelle tarvitaan otsikko');
+define('ERROR_NEWSLETTER_MODULE', 'Virhe: Uutiskirjeen moduli tarvitaan');
+?>
