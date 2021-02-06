3ed7f25590124d126755c102b9e7eb810b5bddb1
Ihsan Yüz <krbuk@nida.fi>
2021-02-06 17:20:09 +0200

    update

--- Finnish_Language_pack_1_5_7/zc_plugins/DisplayLogs/v3.0.0/admin/includes/languages/finnish/display_logs.php
+++ Finnish_Language_pack_1_5_7/zc_plugins/DisplayLogs/v3.0.0/admin/includes/languages/finnish/display_logs.php
@@ -4,34 +4,34 @@
 //
 // Copyright (c) 2012-2017, Vinos de Frutas Tropicales (lat9)
 //
-define('HEADING_TITLE', 'Display Debug Log Files');
+define('HEADING_TITLE', 'Näytä virheenkorjauslokitiedostot ');
 
-define('TABLE_HEADING_FILENAME', 'File Name');
-define('TABLE_HEADING_MODIFIED', 'Date Modified');
-define('TABLE_HEADING_FILESIZE', 'File Size (bytes)');
-define('TABLE_HEADING_DELETE', 'Delete?');
-define('TABLE_HEADING_ACTION', 'Action');
+define('TABLE_HEADING_FILENAME', 'Tiedoston nimi');
+define('TABLE_HEADING_MODIFIED', 'Muokattu');
+define('TABLE_HEADING_FILESIZE', 'Tiedoston koko (bytes)');
+define('TABLE_HEADING_DELETE', 'Poistaa ?');
+define('TABLE_HEADING_ACTION', 'Toiminta');
 
-define('BUTTON_DELETE_SELECTED', 'Delete Selected');
-define('DELETE_SELECTED_ALT', 'Delete all selected files');
-define('BUTTON_DELETE_ALL', 'Delete All');
-define('DELETE_ALL_ALT', 'Delete all files in the current view');
+define('BUTTON_DELETE_SELECTED', 'Poista valitut');
+define('DELETE_SELECTED_ALT', 'Poista kaikki valitut tiedostot ');
+define('BUTTON_DELETE_ALL', 'Poista kaikki');
+define('DELETE_ALL_ALT', 'Poista kaikki nykyisen näkymän tiedostot ');
 
-define('ICON_INFO_VIEW', 'View the contents of this file');
+define('ICON_INFO_VIEW', 'Tarkastele tämän tiedoston sisältöä ');
 
-define('DISPLAY_DEBUG_LOGS_ONLY', 'Display debug-logs only?');
+define('DISPLAY_DEBUG_LOGS_ONLY', 'Näytetäänkö vain virheenkorjauslokeja? ');
 define('LOG_SORT_ASC', 'Asc');
 define('LOG_SORT_DESC', 'Desc');
 
-define('TEXT_HEADING_INFO', 'File Contents');
+define('TEXT_HEADING_INFO', 'Tiedoston sisältö ');
 
 // -----
 // Sort-order descriptions, used in the instructions' display.
 //
-define('TEXT_MOST_RECENT', 'most recent');
-define('TEXT_OLDEST', 'oldest');
-define('TEXT_SMALLEST', 'smallest');
-define('TEXT_LARGEST', 'largest');
+define('TEXT_MOST_RECENT', 'viimeisin');
+define('TEXT_OLDEST', 'vanhin ');
+define('TEXT_SMALLEST', 'pienin ');
+define('TEXT_LARGEST', 'suurin');
 
 // -----
 // The TEXT_INSTRUCTIONS string is passed into sprintf to produce the instructions given on the plugin's main display,
@@ -45,12 +45,12 @@ define('TEXT_LARGEST', 'largest');
 // %6$s ... The "excluded" prefixes for the log-files displayed.
 //
 $imageName = zen_image (DIR_WS_IMAGES . 'icon_info.gif', ICON_INFO_VIEW);
-define('TEXT_INSTRUCTIONS', '<br /><br />The files can be sorted in either ascending or descending order (based on either the last-modified date or the file-size) by clicking on one of the <em>Asc</em> or <em>Desc</em> links. Click on an ' . $imageName . ' icon to view the contents of the associated file.  Only the first %1$u bytes of the selected file will be read; if a file is &quot;over-sized&quot;, its <em>File Size</em> will be highlighted like <span class="bigfile">this</span>.<br /><br />Clicking the <strong>delete all</strong> button will delete all files currently being viewed; clicking <strong>delete selected</strong> will delete only those files with checked checkboxes.<br /><br />Currently viewing the %2$s %3$u of %4$u log files with these <code>%5$s</code> prefixes and <b>not</b> matching these <code>%6$s</code>.<br />');
+define('TEXT_INSTRUCTIONS', '<br /><br />Tiedostot voidaan lajitella nousevassa tai laskevassa järjestyksessä (joko viimeksi muokatun päivämäärän tai tiedostokoon perusteella) napsauttamalla yhtä  <em>Asc</em> or <em>Desc</em> linkkejä . Napsauta  ' . $imageName . ' -kuvaketta nähdäksesi liitetyn tiedoston sisällön. Vain ensimmäinen  %1$u valitun tiedoston tavut luetaan ; jos tiedosto on  &quot;liian suuri&quot;, sen <em>Tiedoston koko</em> korostetaan kuten  <span class="bigfile">tämä</span>.<br /><br />Napsauttamalla  <strong>poista kaikki </strong> -painike poistaa kaikki tarkasteltavat tiedostot ; napsauttamalla  <strong>poista valitut</strong> poistaa vain ne tiedostot, joissa on valintaruudut. <br /><br />Katselet tällä hetkellä  %2$s %3$u / %4$u lokitiedostot näiden kanssa  <code>%5$s</code> etuliitteet ja  <b>not</b> näitä vastaavia <code>%6$s</code>.<br />');
 
-define('JS_MESSAGE_DELETE_ALL_CONFIRM', 'Are you sure you want to delete these \'+n+\' files?');
-define('JS_MESSAGE_DELETE_SELECTED_CONFIRM', 'Are you sure you want to delete the \'+selected+\' selected file(s)?');
+define('JS_MESSAGE_DELETE_ALL_CONFIRM', 'Haluatko varmasti poistaa nämä  \'+n+\' tiedostot?');
+define('JS_MESSAGE_DELETE_SELECTED_CONFIRM', 'Haluatko varmasti poistaa  \'+valittu +\' valittu  tiedosto(t)?');
 
-define('WARNING_NOT_SECURE','<span class="errorText">NOTE: You do not have SSL enabled. File contents you view from this page will not be encrypted and could present a security risk.</span>');
-define('WARNING_NO_FILES_SELECTED', 'No files were selected for deletion!');
-define('WARNING_SOME_FILES_DELETED', 'Warning: Only %u of %u log files were deleted; check permissions.');
-define('SUCCESS_FILES_DELETED', '%u log files were deleted.');
+define('WARNING_NOT_SECURE','<span class="errorText">HUOMAUTUS: SSL ei ole käytössä. Tältä sivulta katsomasi tiedostosisältöä ei salata ja se voi aiheuttaa tietoturvariskin. </span>');
+define('WARNING_NO_FILES_SELECTED', 'Yhtään tiedostoa ei valittu poistettavaksi! ');
+define('WARNING_SOME_FILES_DELETED', 'Varoitus: Vain  %u / %u lokitiedostot poistettiin; tarkista käyttöoikeudet.');
+define('SUCCESS_FILES_DELETED', '%u lokitiedostot poistettiin.');
--- Finnish_Language_pack_1_5_7/zc_plugins/DisplayLogs/v3.0.0/admin/includes/languages/finnish/extra_definitions/display_logs_name.php
+++ Finnish_Language_pack_1_5_7/zc_plugins/DisplayLogs/v3.0.0/admin/includes/languages/finnish/extra_definitions/display_logs_name.php
@@ -4,7 +4,7 @@
 //
 // Copyright (c) 2012-2016, Vinos de Frutas Tropicales (lat9)
 //
-define ('BOX_TOOLS_DISPLAY_LOGS', 'Display Log Files');
+define ('BOX_TOOLS_DISPLAY_LOGS', 'Näytä lokitiedostot ');
 
 // -----
 // This message is displayed in the admin header if any debug-logs are present.  When translating this message, be sure to keep the following "sprintf" tokens:
@@ -12,4 +12,4 @@ define ('BOX_TOOLS_DISPLAY_LOGS', 'Display Log Files');
 // %1$u ... Identifies the number of files present
 // %2$s ... Contains the href to your admin's display-logs tool.
 //
-define ('DISPLAY_LOGS_MESSAGE_LOGS_PRESENT', '%1$u debug-log files exist, click <a href="%2$s">here</a> to view.');
+define ('DISPLAY_LOGS_MESSAGE_LOGS_PRESENT', '%1$u virheenkorjauslokitiedostot ovat olemassa , <a href="%2$s">here</a> katso.');
